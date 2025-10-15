8ee4ad492a0e186066cf6b344a044e9133dd84f0
Inyoung Cho <cupshop123@gmail.com>
2025-10-15 10:46:01 +0900

    feat: 쉼표와 콜론 구분자로 문자열 덧셈 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 1. 값 입력 받기
+    const input = await Console.readLineAsync("값을 입력해주세요: ");
+
+    // 2. 쉼표와 클론 구분자 제거
+    const numbers = input.split(/[,:]/).map(Number);
+
+    // 3. 숫자 더하기
+    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+
+    // 4. 결과 출력
+    Console.print(`결과: ${sum}`);
+  }
 }
 
 export default App;
