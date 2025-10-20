78cffc30bd9ff516fc37c0c6f8c590529a997f75
Inyoung Cho <cupshop123@gmail.com>
2025-10-20 13:16:12 +0900

    feat: 커스텀 구분자 처리

--- src/App.js
+++ src/App.js
@@ -5,13 +5,31 @@ class App {
     // 1. 값 입력 받기
     const input = await Console.readLineAsync("값을 입력해주세요: ");
 
-    // 2. 쉼표와 클론 구분자 제거
-    const numbers = input.split(/[,:]/).map(Number);
+    if (input === "") {
+      Console.print("결과: 0");
+      return;
+    }
 
-    // 3. 숫자 더하기
-    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+    // 2. 커스텀 구분자 처리
+    // \n을 줄바꿈 -> 실제 문자로 변환
+    const normalizedInput = input.replace(/\\n/g, "\n");
 
-    // 4. 결과 출력
+    // 커스텀 구분자 추출
+    const regex = /^\/\/(.)\n/;
+    const match = normalizedInput.match(regex);
+
+    // 숫자만 담은 배열
+    let numbers;
+
+    if (match) {
+      const matchInput = normalizedInput.replace(match[0], ""); // match[0]: ex.//;\n
+      numbers = matchInput.split(match[1]); // [1,2,3]
+    } else {
+      // 3. 쉼표와 클론 구분자 처리
+      numbers = normalizedInput.split(/[,:]/); // [1,2,3]
+    }
+
+    let sum = numbers.reduce((acc, cur) => acc + Number(cur), 0);
     Console.print(`결과: ${sum}`);
   }
 }
